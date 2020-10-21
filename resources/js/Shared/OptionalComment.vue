6a603dd71cc23191bfcd01e544ea35e5672e5d9b
Chester Martinez <chestermartinez@ormindoro.gov.ph>
2020-10-21 10:03:50 +0800

    adjusted feedback ui

--- resources/js/Shared/OptionalComment.vue
+++ resources/js/Shared/OptionalComment.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <div class="table w-full text-xl md:text-4xl">
+  <div class="w-10/12">
+    <div class="table w-full text-xl table-fixed md:px-6 md:text-3xl">
       <div
         class="tr"
         v-for="(question,i) in questions"
         :key="question.id"
       >
-        <div class="justify-center flex-none p-2 td md:p-3">
+        <div class="justify-start flex-initial p-1 pr-2 md:p-2 td md:pr-6">
           <input
             type="checkbox"
             :id="`positive${i+1}`"
@@ -16,8 +16,8 @@
             class="md:zoom-20 zoom-15"
           >
         </div>
-        <div class="p-2 flex-grow-1 td md:p-3 md:pl-6">
-          {{ question.question }}
+        <div class="p-1 md:p-2 flex-grow-1 td md:p-3">
+          <p>{{ question.question }}</p>
         </div>
       </div>
     </div>
