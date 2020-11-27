d24a808392761d661bd66ddf3281ae307e1b9cce
Chester Martinez <chestermartinez@ormindoro.gov.ph>
2020-11-27 10:11:56 +0800

    done dummy table

--- resources/js/Pages/Reports/DummyTable.vue
+++ resources/js/Pages/Reports/DummyTable.vue
@@ -8,56 +8,86 @@
     >
       <tr>
         <td
-          class="p-2"
-          rowspan="3"
+          class="px-2 py-1 align-top border border-black"
+          rowspan="6"
         >
-          <div class="h-6 bg-gray-300 rounded animate-pulse" />
+          <div
+            class="h-5 bg-gray-300 rounded animate-pulse"
+            :class="randomWidthClass()"
+          />
         </td>
-        <td class="w-5/12 p-2">
-          <div class="w-6/12 h-6 bg-gray-300 rounded animate-pulse" />
+        <td class="w-5/12 px-2 py-1 border border-t-0">
+          <div class="w-7/12 h-5 bg-gray-300 rounded animate-pulse" />
         </td>
         <td
-          class="p-2"
+          class="px-2 py-1 border border-b border-r-0 border-black"
           rowspan="3"
         >
-          <div class="w-6/12 h-6 m-auto bg-gray-300 rounded animate-pulse" />
+          <div class="w-1/4 h-5 m-auto bg-gray-300 rounded animate-pulse" />
         </td>
         <td
-          class="p-2 "
+          class="px-2 py-1 border "
           v-for="y in 5"
           :key="'dummy'+y+Math.random()"
         >
-          <div class="h-6 bg-gray-300 rounded animate-pulse" />
+          <div class="h-5 bg-gray-300 rounded animate-pulse" />
         </td>
         <td
-          class="p-2"
+          class="px-2 py-1 border border-black"
           rowspan="3"
         >
-          <div class="h-6 m-auto bg-gray-300 rounded animate-pulse" />
+          <div class="w-1/2 h-5 m-auto bg-gray-300 rounded animate-pulse" />
         </td>
       </tr>
       <tr>
-        <td class="p-2">
-          <div class="w-8/12 h-6 bg-gray-300 rounded animate-pulse" />
+        <td class="px-2 py-1 border">
+          <div class="w-3/4 h-5 bg-gray-300 rounded animate-pulse" />
         </td>
         <td
-          class="p-2 "
+          class="px-2 py-1 border"
           v-for="y in 5"
           :key="'dummy2'+y+Math.random()"
         >
-          <div class="h-6 bg-gray-300 rounded animate-pulse" />
+          <div class="h-5 bg-gray-300 rounded animate-pulse" />
         </td>
       </tr>
       <tr>
-        <td class="p-2">
-          <div class="h-6 bg-gray-300 rounded animate-pulse w-12/12" />
+        <td class="px-2 py-1 border border-l-0 border-r-0 border-black">
+          <div class="w-11/12 h-5 bg-gray-300 rounded animate-pulse" />
         </td>
         <td
-          class="p-2 "
+          class="px-2 py-1 border border-l-0 border-r-0 border-black"
           v-for="y in 5"
           :key="'dummy2'+y+Math.random()"
         >
-          <div class="h-6 bg-gray-300 rounded animate-pulse" />
+          <div class="h-5 bg-gray-300 rounded animate-pulse" />
+        </td>
+      </tr>
+      <tr>
+        <td
+          class="px-2 py-1 border border-black"
+          colspan="9"
+        >
+          <div class="w-full h-5 bg-yellow-300 rounded animate-pulse" />
+        </td>
+      </tr>
+      <tr>
+        <td
+          class="px-2 py-1 border border-black"
+          colspan="9"
+        >
+          <div class="w-full h-8 bg-gray-300 rounded animate-pulse" />
+        </td>
+      </tr>
+      <tr>
+        <td class="px-2 py-1">
+          <div class="w-full h-5 bg-gray-300 rounded animate-pulse" />
+        </td>
+        <td
+          class="px-2 py-1 border border-black"
+          colspan="7"
+        >
+          <div class="w-full h-5 bg-gray-300 rounded animate-pulse" />
         </td>
       </tr>
     </tbody>
@@ -74,6 +104,11 @@ export default {
       type: Number,
       default: 1
     }
-  }
+  },
+  methods: {
+    randomWidthClass(){
+      return 'w-'+(Math.floor(Math.random() * (12 - 3 + 1)) + 3)+'/12';
+    }
+  },
 }
 </script>
