4c07ba2afadf76423ef737c48b8d83802c978920
Chester Martinez <martinez@greenova.de>
2022-05-17 23:23:53 +0200

    update for phpstorm

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
-        App\Providers\JetstreamServiceProvider::class
+        App\Providers\JetstreamServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
